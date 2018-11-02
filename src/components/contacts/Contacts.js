5ed0b64575e0ec1e3dac7aa785bd42f37f61f370
Pushkar Singh <theagilecoder@gmail.com>
2018-11-02 18:08:09 +0530

    Refactor - Create separate Actions file

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
 import { connect } from "react-redux";
-import { GET_CONTACTS } from "../../actions/types";
+import { getContacts } from "../../actions/contactActions";
 import PropTypes from "prop-types";
 
 class Contacts extends Component {
@@ -33,11 +33,7 @@ const mapStateToProps = state => ({
   contacts: state.contact.contacts
 });
 
-const mapDispatchToProps = dispatch => ({
-  getContacts: () => dispatch({ type: GET_CONTACTS })
-});
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  { getContacts }
 )(Contacts);
